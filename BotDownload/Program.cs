3bcb5e0f6ac3e17cb968d735e415732fe38f7772
Leandro Diomar <leandrodiomar123@gmail.com>
2022-09-28 22:38:33 -0300

    Adicao download arquivo via selenium

--- BotDownload/Program.cs
+++ BotDownload/Program.cs
@@ -1,14 +1,19 @@
-﻿internal class Program
+﻿using BotDownload.Services;
+using System.IO;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
-        string caminhoResources , caminhoArquivosZipados, caminhoArquivosExtraidos;
+        string caminhoResources , caminhoArquivosZipados, caminhoArquivosExtraidos, caminhoWebDriver;
         caminhoResources = Path.GetDirectoryName(typeof(Program).Assembly.Location);
         caminhoResources = caminhoResources.Split(@"BotDownload\")[0] + @"BotDownload\Resources";
 
-        caminhoArquivosZipados = caminhoResources + @"\ArquivosZipados";
-        caminhoArquivosExtraidos = caminhoResources + @"\ArquivosExtraidos";
+        caminhoArquivosZipados = Path.Combine(caminhoResources,"ArquivosZipados");
+        caminhoArquivosExtraidos = Path.Combine(caminhoResources,"ArquivosExtraidos");
+        caminhoWebDriver = Path.Combine(caminhoResources,"WebDrivers");
 
+        //Criar pastas
         if (!Directory.Exists(caminhoArquivosZipados))
         {
             Directory.CreateDirectory(caminhoArquivosZipados);
@@ -18,8 +23,18 @@
             Directory.CreateDirectory(caminhoArquivosExtraidos);
         }
 
+        //baixar arquivo
+        if (File.Exists(Path.Combine(caminhoArquivosZipados, "DADOS_ABERTOS_CNPJ_01.zip")))
+        {
+            DownloadService downloadService = new DownloadService();
 
-        
+            downloadService.BaixarArquivosCnpj(caminhoWebDriver, caminhoArquivosZipados);
+
+        }
+
+
+
+
 
     }
 }
--- BotDownload/Services/DownloadService.cs
+++ BotDownload/Services/DownloadService.cs
@@ -1,23 +1,42 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Text;
-using System.Threading.Tasks;
+﻿using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using SeleniumExtras.WaitHelpers;
 
 namespace BotDownload.Services
 {
     internal class DownloadService
     {
 
-     
+        private const string linkArquivoCnpj = "https://drive.google.com/uc?export=download&id=11JEE8WKSD9_FBAfGfiFq_z-ZtS1bmGeR";
 
-        private void BaixarArquivosCnpj(string path, string link)
+        public void BaixarArquivosCnpj(string pathDriver, string pathDownload)
         {
-            
+
+            string btnDownload = "uc-download-link";
+            bool pastaCriada;
+
+            ChromeOptions options = new ChromeOptions();
+            options.AddUserProfilePreference("download.default_directory", pathDownload);
+
             try
             {
-                
+                IWebDriver driver = new ChromeDriver(pathDriver, options);
+                WebDriverWait waitElements = new WebDriverWait(driver, TimeSpan.FromSeconds(60));
+                WebDriverWait waitDownload = new WebDriverWait(driver, TimeSpan.FromSeconds(500));
+
+
+                driver.Navigate()
+                    .GoToUrl(linkArquivoCnpj);
+
+                waitElements.Until(ExpectedConditions.ElementExists(By.Id(btnDownload)));
+
+                driver.FindElement(By.Id(btnDownload)).Click();
+
+                waitDownload.Until( _ => pastaCriada = File.Exists(Path.Combine(pathDownload, "DADOS_ABERTOS_CNPJ_01.zip")));
+
+                driver.Quit();
+
             }
             catch (Exception e)
             {
